dcb1eb8d61844105a735dae4deb4a9a80d879b7a
leehonghwa <lyhonghwa@gmail.com>
2017-11-28 22:54:28 +0900

    백그라운드 상태일때 데이터를 저장하게 변경

--- DogDay/DogDay/Model/DogDay.swift
+++ DogDay/DogDay/Model/DogDay.swift
@@ -145,22 +145,20 @@ struct DogDay: Codable {
         switch index {
         case DogDayType.heartWorm.rawValue:
             self.dogDayType = .heartWorm
-            break
+            
         case DogDayType.pill.rawValue:
             self.dogDayType = .pill
-            break
+            
         case DogDayType.heart.rawValue:
             self.dogDayType = .heart
-            break
+            
         case DogDayType.vaccination.rawValue:
             self.dogDayType = .vaccination
-            break
+            
         case DogDayType.beauty.rawValue:
             self.dogDayType = .beauty
-            break
             
         default:
-            
             break
         }
     }
@@ -185,13 +183,7 @@ final class DogDays: Codable {
         var tempElement = element
         tempElement.startTime = Date()
         self.items.append(tempElement)
-        self.items.sort { (first, second) -> Bool in
-            if let first = first.fullEndDate, let second = second.fullEndDate {
-                return first < second
-            }
-            return true
-        }
-        self.encoded()
+        self.sortItems()
         postUpdateDataNotification()
     }
     
@@ -209,10 +201,20 @@ final class DogDays: Codable {
     public func editDogDayElement(at index: Int, newElement:DogDay) {
         self.items.remove(at: index)
         self.items.insert(newElement, at: index)
+        self.sortItems()
         postUpdateDataNotification()
     }
     
-    func encoded() {
+    private func sortItems() {
+        self.items.sort { (first, second) -> Bool in
+            if let first = first.fullEndDate, let second = second.fullEndDate {
+                return first < second
+            }
+            return true
+        }
+    }
+    
+    public func encoded() {
         do {
             let dogDayData = try JSONEncoder().encode(self)
             try dogDayData.write(to: DogDays.encodingPath())
